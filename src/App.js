7321d0f947532ebc66b3d772cd0b0e4bbc906056
Mayarrrrr <mayar.ateffff@gmail.com>
2023-10-10 00:20:17 +0300

    deploy version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Home from './components/home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route , HashRouter} from "react-router-dom";
 import NavBar from './components/navbar/NavBar';
 import Blogs from './components/blogs/Blogs';
 
@@ -9,12 +9,12 @@ function App() {
   return (
     <div className="App">
       <Home/>
-      <BrowserRouter>
+      <HashRouter>
       <Routes>
           <Route exact path="/" element={<Home/>} />
           <Route exact path="/Blogs" element={<Blogs/>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
     </div>
   );
 }
